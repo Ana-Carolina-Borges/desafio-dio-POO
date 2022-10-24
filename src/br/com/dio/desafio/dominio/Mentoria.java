e6a2b15279beac83c5bc6c5ab13e95c595288db8
Ana Carolina <carolbd1107@gmail.com>
2022-10-24 20:50:09 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,51 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    String titulo;
+    String descricao;
+    LocalDate data;
+
+    public Mentoria() {
+        this.titulo = titulo;
+    }
+
+    public Mentoria(String titulo, String descricao, LocalDate data) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data = data;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria - " +
+                "Titulo= " + titulo +
+                " - Descricao= " + descricao +
+                " - Data= " + data;
+    }
+}
